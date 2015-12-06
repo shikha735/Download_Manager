a21cdc565744de75d83b17b05456a3d9e45d15af
Shikha Sharma <shikha735@gmail.com>
2015-12-06 23:27:10 +0530

    made changes in ProgressRenderer file

--- ProgressRenderer.java
+++ ProgressRenderer.java
@@ -8,4 +8,11 @@ class ProgressRenderer extends JProgressBar implements TableCellRenderer {
 	public ProgressRenderer(int min, int max){
 		super(min, max);	
 	}
+	
+	// Returns JProgressBar as renderer for the given table cell
+	public Component getTableCellRendererComponent(Jtable table, Object value, boolean isSelected, boolean hasFocus, int row, int column) {
+		// Setting the progress bar's value
+		setValue((int) ((Float)value).floatValue());
+		return this;
+	}
 }
